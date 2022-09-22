d5bfc3e6101318ca299f4c819f9c0927cb1e58f4
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-09-22 16:25:57 +0200

    add a function to be able to get a camp by id

--- routes/camp.js
+++ routes/camp.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/camp');
+const { create, campById, read } = require('../controllers/camp');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
+router.get('/camp/:campId', read);
 router.post('/camp/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 router.param('userId', userById);
+router.param('campId', campById);
 
 module.exports = router;
