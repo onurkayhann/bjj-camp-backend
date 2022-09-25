826d0cfa449af3969ca4c2c154a38674a6fae01c
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-09-25 18:00:03 +0200

    add CORS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 require('dotenv').config();
 
@@ -26,6 +27,7 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use('/api', authRoutes);
