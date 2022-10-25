8bf84141541e146f009db21985aa5f3b4da6b2ff
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-10-25 19:14:56 +0200

    add listOrders so that admin can view all the orders in the dashboard

--- controllers/order.js
+++ controllers/order.js
@@ -14,3 +14,17 @@ exports.create = (req, res) => {
     res.json(data);
   });
 };
+
+exports.listOrders = (req, res) => {
+  Order.find()
+    .populate('user', '_id name belt_color')
+    .sort('-created')
+    .exec((err, orders) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(error),
+        });
+      }
+      res.json(orders);
+    });
+};
