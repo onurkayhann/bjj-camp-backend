1fde4d3bfb448dea79c2c89185ed87c382ec7cac
Onur Kayhan <onur.kayhan@chasacademy.se>
2022-11-03 10:01:52 +0100

    nothing

--- controllers/user.js
+++ controllers/user.js
@@ -87,31 +87,12 @@ exports.bookingHistory = (req, res) => {
 
 // user GET
 exports.list = (req, res) => {
-  // let order = req.query.order ? req.query.order : 'asc';
-  // let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
-  // let limit = req.query.limit ? parseInt(req.query.limit) : 6;
-
-  User.find()
-  .exec((err, users) => {
+  User.find().exec((err, users) => {
     if (err) {
       return res.status(400).json({
-        error: 'Users not found'
-      })
+        error: 'Users not found',
+      });
     }
     res.json(users);
-  })
-  };
-
-  // User.find()
-  //   .select('-name')
-  //   .populate('category')
-  //   .sort([[sortBy, order]])
-  //   .limit(limit)
-  //   .exec((err, users) => {
-  //     if (err) {
-  //       return res.status(400).json({
-  //         error: 'Camp not found',
-  //       });
-  //     }
-  //     res.json(users);
-  //   });
+  });
+};
